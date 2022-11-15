461d7eec630ba6cd33e7cc8508d940726c02ea48
josemcaba <96978309+josemcaba@users.noreply.github.com>
2022-11-15 03:38:32 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("IoT", "a1b2c3d4>>")
+ESP8266_IoT.connectThingSpeak()
+basic.forever(function () {
+    ESP8266_IoT.setData(
+    "JBQ3F7SI645YFNT7",
+    Environment.ReadLightIntensity(AnalogPin.P1)
+    )
+    ESP8266_IoT.uploadData()
+    basic.pause(60000)
+})
